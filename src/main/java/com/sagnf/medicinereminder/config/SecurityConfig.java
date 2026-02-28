7fe7cc05084ca1e1143ac4149678205c150273b3
Abdul Gohar <shaikabdulgohar3@gmail.com>
2026-02-28 11:39:37 +0530

    secuirityconfi-csrf update2

--- src/main/java/com/sagnf/medicinereminder/config/SecurityConfig.java
+++ src/main/java/com/sagnf/medicinereminder/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.sagnf.medicinereminder.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -22,7 +24,6 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/",
                                 "/api/user/register",
                                 "/api/user/login"
                         ).permitAll()
@@ -32,6 +33,12 @@ public class SecurityConfig {
                 .formLogin(form -> form.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
